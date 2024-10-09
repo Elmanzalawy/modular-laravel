dcb9a49b28f10404af61afe504a8411391a1a0ef
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2024-10-09 21:14:36 +0300

    add product factory & migration

---
+++ modules/Product/Database/Factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Product\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Product\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Product\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'price' => fake()->randomFloat(min: 15, max: 200),
+            'in_stock' => fake()->boolean(),
+        ];
+    }
+}
